a77385249bbd2395798449d9286e1aa6d1b9c7b8
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-13 20:15:25 +0800

    Trying to put designs

--- TestTV.py
+++ TestTV.py
@@ -4,6 +4,9 @@ from ClassTV import TV
 TV1 = TV("tv1's", '30', '3')
 TV2 = TV("tv2's", '3', '2')
 
+
+from art import *
+
 # Call methods for output
-TV1.getChannel(), TV1.getVolume()
-TV2.getChannel(), TV2.getVolume()
+Filename = tsave(TV1.getChannel(), TV1.getVolume(), '\n', TV2.getChannel(), TV2.getVolume(), "ClassTV.py")
+print(Filename)
