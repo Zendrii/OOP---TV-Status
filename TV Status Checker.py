7ee074599f72bc9c63cb73a9debe5c459cf93133
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-13 19:08:46 +0800

    Removed getChannel2 & getVolume2 because of realization that itis way simpler than initially planned

--- TV Status Checker.py
+++ TV Status Checker.py
@@ -1,12 +1,10 @@
 # Create Class named "TV"
 class TV:
-    def __init__(self, tv, channel, volume, channelTwo, volumeTwo):
+    def __init__(self, tv, channel, volume):
         # instance variables
         self.tv = tv
         self.channel = channel
         self.volume = volume
-        self.channelTwo = channelTwo
-        self.volumeTwo = volumeTwo
     
     # Create Behaviors/Statements
     def getChannel(self):
@@ -14,16 +12,10 @@ class TV:
     def getVolume(self):
         print(' and volume level is ', self.volume)
 
-    def getChannel2(self):
-        print("TV2's channel is ", self.channelTwo)
-    def getVolume2(self):
-        print(' and volume level is ', self.volumeTwo)
-        
-
 # Create Objects for created class
 TV1 = TV('30', '3', '0', '0')
 TV2 = TV('0', '0', '3', '2')
 
 # Call methods for output
 TV1.getChannel, TV1.getVolume
-TV2.getChannel2, TV2.getVolume2
+TV2.getChannel, TV2.getVolume
