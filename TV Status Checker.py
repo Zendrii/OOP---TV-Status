10359694eff8564fd51820105a9d4ab3693beb9d
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-13 18:37:02 +0800

    Fixing the objects and creating the callout for the behaviors

--- TV Status Checker.py
+++ TV Status Checker.py
@@ -11,14 +11,19 @@ class TV:
     def getChannel(self):
         print("TV1's channel is ", self.channel)
     def getVolume(self):
-        print(' and volume level is ', self.volumeTwo)
+        print(' and volume level is ', self.volume)
 
     def getChannel2(self):
         print("TV2's channel is ", self.channelTwo)
     def getVolume2(self):
         print(' and volume level is ', self.volumeTwo)
         
-# Create Objects for created class
-TV1 = TV('30', '3')
-TV2 = TV('0', '0', '3', '2')
-# Call methods for output
+def TestTV():
+    # Create Objects for created class
+    TV1 = TV('30', '3')
+    # Call methods for output
+    TV1.getChannel, TV1.getVolume
+    # Create Objects for created class
+    TV2 = TV('0', '0', '3', '2')
+    # Call methods for output
+    TV2.getChannel2, TV2.getVolume2
