e7eb8f800e7edb37e997d440ed8081013bdca6e9
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-13 18:20:52 +0800

    Fixing the instance variables and creating methods/statements

--- TV Status Checker.py
+++ TV Status Checker.py
@@ -1,9 +1,21 @@
 # Create Class named "TV"
 class TV:
-    def __init__(self, channel, volume):
+    def __init__(self, channel, volume, channelTwo, volumeTwo):
         # instance variables
         self.channel = channel
         self.volume = volume
-# Create Behaviors/Staements
+        self.channelTwo = channelTwo
+        self.volumeTwo = volumeTwo
+    
+    # Create Behaviors/Staements
+    def getChannel(self):
+        print("TV1's channel is ", self.channel)
+    def getVolume(self):
+        print(' and volume level is ', self.volumeTwo)
+
+    def getChannel2(self):
+        print("TV2's channel is ", self.channelTwo)
+    def getVolume2(self):
+        print(' and volume level is ', self.volumeTwo)
 # Create Objects for created class
 # Call methods for output
