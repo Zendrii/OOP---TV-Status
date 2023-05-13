c3a829ea8ff00719aea8f1167eabb985aaf68813
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-13 17:56:55 +0800

    Class TV and its instance variables created

--- TV Status Checker.py
+++ TV Status Checker.py
@@ -1,4 +1,9 @@
 # Create Class named "TV"
+class TV:
+    def __init__(self, channel, volume):
+        # instance variables
+        self.channel = channel
+        self.volume = volume
 # Create Behaviors/Staements
 # Create Objects for created class
 # Call methods for output
