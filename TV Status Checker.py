102b0867e54dc2e05fc65937e7bdf44cce331d78
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-13 19:05:16 +0800

    Added another instance variable to make it more compact

--- TV Status Checker.py
+++ TV Status Checker.py
@@ -1,7 +1,8 @@
 # Create Class named "TV"
 class TV:
-    def __init__(self, channel, volume, channelTwo, volumeTwo):
+    def __init__(self, tv, channel, volume, channelTwo, volumeTwo):
         # instance variables
+        self.tv = tv
         self.channel = channel
         self.volume = volume
         self.channelTwo = channelTwo
@@ -9,7 +10,7 @@ class TV:
     
     # Create Behaviors/Statements
     def getChannel(self):
-        print("TV1's channel is ", self.channel)
+        print("'s channel is ", self.channel)
     def getVolume(self):
         print(' and volume level is ', self.volume)
 
