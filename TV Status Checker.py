79476f96c5689ee3a314e37118162f574ec84a62
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-13 19:37:45 +0800

    Fixing the spacings

--- TV Status Checker.py
+++ TV Status Checker.py
@@ -8,13 +8,13 @@ class TV:
     
     # Create Behaviors/Statements
     def getChannel(self):
-        print(self.tv, "'s channel is ", self.channel, end='')
+        print(self.tv, "channel is", self.channel, end='')
     def getVolume(self):
-        print(' and volume level is ', self.volume)
+        print(' and volume level is', self.volume)
 
 # Create Objects for created class
-TV1 = TV('tv1', '30', '3')
-TV2 = TV('tv2', '3', '2')
+TV1 = TV("tv1's", '30', '3')
+TV2 = TV("tv2's", '3', '2')
 
 # Call methods for output
 TV1.getChannel(), TV1.getVolume()
