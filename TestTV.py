00eed1cfcb9b8829995d3c0f0375ccbad45505a2
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-13 20:25:17 +0800

    Settled for pyfiglet design

--- TestTV.py
+++ TestTV.py
@@ -4,9 +4,10 @@ from ClassTV import TV
 TV1 = TV("tv1's", '30', '3')
 TV2 = TV("tv2's", '3', '2')
 
-
-from art import *
+import pyfiglet
 
 # Call methods for output
-Filename = tsave(TV1.getChannel(), TV1.getVolume(), '\n', TV2.getChannel(), TV2.getVolume(), "ClassTV.py")
-print(Filename)
+Output = pyfiglet.figlet_format(TV1.getChannel(), TV1.getVolume(), '\n', TV2.getChannel(), TV2.getVolume(), font = "slant" )
+
+# Print output
+print(Output)
