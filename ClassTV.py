ccfba934114ccf09b475ba07f7c1cc2b1c19ef16
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-14 13:50:23 +0800

    Visual updates and the ability to change the channel 1 up or down

--- ClassTV.py
+++ ClassTV.py
@@ -10,15 +10,28 @@ class TV:
     # Create Behaviors/Statements
     def turnOn(self):
         self.power = True
+
     def turnOff(self):
         self.power = False
+        
     def getChannel(self):
         print(self.tv, "channel is", self.channel, end='')
+
     def setChannel(self):
         self.channel = input('Set the channel you want: ')
         print('You have set the channel to', self.channel)
+
     def getVolume(self):
         print(' and volume level is', self.volume)
+
     def setVolume(self):
         self.volume = input('Set the volume level you want: ')
         print('You have set the volume level to', self.volume)
+
+    def channelUp(self):
+        channel_change = self.channel + 1
+        print('You have changed the channel to', channel_change)
+
+    def channelDown(self):
+        channel_change2 = self.channel - 1
+        print('You have changed the channel to', channel_change2)
