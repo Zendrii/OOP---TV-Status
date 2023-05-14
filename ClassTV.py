fab69629b5e905ca2a4e15bb9c6a26cca1b0e00f
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-14 13:33:04 +0800

    Adding the power variable and its behaviors

--- ClassTV.py
+++ ClassTV.py
@@ -1,12 +1,17 @@
 # Create Class named "TV"
 class TV:
-    def __init__(self, tv, channel, volume):
+    def __init__(self, tv, channel, volume, power):
         # instance variables
         self.tv = tv
         self.channel = channel
         self.volume = volume
+        self.power = power
     
     # Create Behaviors/Statements
+    def turnOn(self):
+        self.power = True
+    def turnOff(self):
+        self.power = False
     def getChannel(self):
         print(self.tv, "channel is", self.channel, end='')
     def getVolume(self):
