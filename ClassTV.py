d8c2f7a31ce31a6ece35b95f049621edc5b72311
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-14 13:42:54 +0800

    Adding the ability to change the channel and volume level

--- ClassTV.py
+++ ClassTV.py
@@ -1,10 +1,10 @@
 # Create Class named "TV"
 class TV:
-    def __init__(self, tv, channel, volume, power):
+    def __init__(self, tv, channel, volumeLevel, power):
         # instance variables
         self.tv = tv
         self.channel = channel
-        self.volume = volume
+        self.volume = volumeLevel
         self.power = power
     
     # Create Behaviors/Statements
@@ -14,5 +14,11 @@ class TV:
         self.power = False
     def getChannel(self):
         print(self.tv, "channel is", self.channel, end='')
+    def setChannel(self):
+        self.channel = input('Set the channel you want: ')
+        print('You have set the channel to', self.channel)
     def getVolume(self):
         print(' and volume level is', self.volume)
+    def setVolume(self):
+        self.volume = input('Set the volume level you want: ')
+        print('You have set the volume level to', self.volume)
